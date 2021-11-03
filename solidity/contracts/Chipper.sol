0e432a31aee2b8a53bc1a64bee9568d5eacaf179
Durendal <durendal@durendals-domain.com>
2021-11-03 15:03:03 -0600

    Added artifacts to gitignore and cleaned up Chipper slightly

--- solidity/contracts/Chipper.sol
+++ solidity/contracts/Chipper.sol
@@ -5,12 +5,6 @@ import './interfaces/IUniswapV2Pair.sol';
 import './interfaces/IERC20.sol';
 
 contract Chipper {
-    struct Pair {
-        uint112 reserve0; 
-        uint112 reserve1;
-        uint32 blockTimestampLast;
-    }
-
     constructor() {
 
     }
@@ -22,12 +16,9 @@ contract Chipper {
     function getPrice(address tokenA, address tokenB) public {
         string memory nameA = IERC20(tokenA).name();
         string memory nameB = IERC20(tokenB).name();
-        uint112 reserve0;
-        uint112 reserve1;
-        uint32 blockTimestampLast;
         address pair = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f).getPair(tokenA, tokenB);
         require(pair != address(0x0), "Invalid pair specified");
-        (reserve0, reserve1, blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
+        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
 
     }
 }
